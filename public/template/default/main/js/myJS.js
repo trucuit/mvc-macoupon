6003ee468017c472767c84b2afda24b5616ae44d
TRỰC TRẦN TRUNG <15520939@gm.uit.edu.vn>
2017-11-22 12:01:50 +0700

    fix xong

--- public/template/default/main/js/myJS.js
+++ public/template/default/main/js/myJS.js
@@ -18,7 +18,31 @@ jQuery(document).ready(function($) {
 
 	controllerName = getUrlVar('controller');
 	$("."+controllerName).addClass('active');
+    $(".close").click(function(event) {
+        $("#mymodal").hide();
+    });//tắt popup
 
+
+    $(".chitiet").click(function(event) {
+
+        var setId=$(this).attr("setId");
+        var setClass = '.content_sale'+setId;
+        var getText = $(setClass).html();
+        var setClassXt='.xemthem'+setId;
+        var compareText= $(setClassXt).text();
+
+        $(setClass).toggleClass('show');
+        if(compareText == '...Xem thêm'){
+            $(setClassXt).empty();
+            $(setClassXt).text('Thu gọn');
+        }
+        else if(compareText == 'Thu gọn'){
+            $(setClassXt).empty();
+            $(setClassXt).text('...Xem thêm');
+        }
+
+
+    });//nút xem thêm
 });
 
 function getUrlVar(key){
@@ -63,3 +87,35 @@ function selectAjax(url,domain){
 	})
 
 }
+//shopping
+function pupUp(url,id){
+    // console.log(this);
+    // console.log(id);
+    $.ajax({
+        url: url,
+        type: 'POST',
+        dataType: 'text',
+        data:{id_get: id},
+    })
+        .done(function(data) {
+            console.log("success");
+            var pos_end=data.search("<br/>");
+            var pos_start=data.search("https");
+            var link=data.substring(pos_start,pos_end);
+            var data_coupon=data.substring(pos_end,);
+            $(".btn_golazada").attr("href",link);
+            $(".copy_coupon").html(data_coupon);
+
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+    $("#mymodal").show();
+
+}
+
+// //shopping
