468d0a83ed1d0e17e6c8f8164d428da89f1dcdee
TRỰC TRẦN TRUNG <15520939@gm.uit.edu.vn>
2017-11-22 12:01:50 +0700

    fix xong

--- application/module/default/views/adayroiShopping/ajaxadayroi.php
+++ application/module/default/views/adayroiShopping/ajaxadayroi.php
@@ -1,17 +1,17 @@
 <?php
 
 $data_edit1=$this->arrAdayroi;
-		if (isset($_POST['id_get'])) {
-			$id=(int)$_POST['id_get'];
-			$arr_coupon=$data_edit1[$id]["coupons"];
-			echo $data_edit1[$id]["aff_link"]."<br/>";
-			if (!empty($arr_coupon)) {
+if (isset($_POST['id_get'])) {
+    $id=(int)$_POST['id_get'];
+    $arr_coupon=$data_edit1[$id]["coupons"];
+    echo $data_edit1[$id]["aff_link"]."<br/>";
+    if (!empty($arr_coupon)) {
 
-				foreach ($arr_coupon as $key => $value) {
-					foreach ($value as $key1 => $value1) {
-						echo $value1."<br/>";
-					}
-				}
-			}
-		}
+        foreach ($arr_coupon as $key => $value) {
+            foreach ($value as $key1 => $value1) {
+                echo $value1."<br/>";
+            }
+        }
+    }
+}
 ?>
